a14b837c49121e83cfe9e450bfd589ea01f9babc
SiPilip <philifs.74hutar@gmail.com>
2025-07-31 23:42:30 +0700

    canvas

---
+++ src/components/navbar.tsx
@@ -0,0 +1,107 @@
+"use client";
+import Link from "next/link";
+import ThemeChanger from "./darkswitch";
+import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import {
+  HiAcademicCap,
+  HiBell,
+  HiMegaphone,
+  HiNewspaper,
+  HiTag,
+} from "react-icons/hi2";
+
+export const Navbar = () => {
+  const navigation = [
+    { name: "Profil Sekolah", href: "/", logo: <HiAcademicCap /> },
+    { name: "Berita", href: "/", logo: <HiNewspaper /> },
+    { name: "Akademik & Dokumen", href: "/", logo: <HiTag /> },
+    { name: "Kegiatan & Informasi", href: "/", logo: <HiBell /> },
+    { name: "PPDB", href: "/", logo: <HiMegaphone /> },
+  ];
+
+  return (
+    <div className="w-full">
+      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-1">
+        {/* Logo  */}
+        <Link href="/">
+          <span className="flex items-center  -space-x-3 font-medium text-black-500 dark:text-gray-100 ">
+            <span>
+              <Image
+                src="/img/logo.svg"
+                width="100"
+                alt="Logo SMA Methodist 1 Palembang"
+                height="100"
+              />
+            </span>
+            <div className="flex flex-col -space-y-1 text-md">
+              <span className="font-semibold">SMA METHODIST 1</span>
+              <span>PALEMBANG</span>
+            </div>
+          </span>
+        </Link>
+
+        {/* Desktop Navigation */}
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList>
+            {navigation.map((item) => (
+              <NavigationMenuItem key={item.name}>
+                <NavigationMenuLink asChild>
+                  <Link
+                    href={item.href}
+                    className={navigationMenuTriggerStyle()}
+                  >
+                    <span className="mr-1 text-lg">{item.logo}</span>
+                    {item.name}
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* Mobile Navigation */}
+        <div className="lg:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Menu className="w-6 h-6" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="right">
+              <div className="grid gap-4 py-4">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="text-lg font-medium"
+                  >
+                    <span className="mr-1 text-md">{item.logo}</span>
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        {/* Theme Changer and Get Started */}
+        <div className="hidden lg:flex items-center gap-3">
+          <ThemeChanger />
+          <Button asChild>
+            <Link href="/">Daftar Sekarang!</Link>
+          </Button>
+        </div>
+      </nav>
+    </div>
+  );
+};
