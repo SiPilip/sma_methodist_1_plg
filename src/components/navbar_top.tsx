578bcf50289bd9023d9ab17964d9fb526479778f
SiPilip <philifs.74hutar@gmail.com>
2025-08-12 16:31:02 +0700

    Memperbarui komponen NavbarTop dengan menambahkan kelas CSS 'no-underline' pada elemen kontak untuk meningkatkan konsistensi gaya dan pengalaman pengguna.

--- src/components/navbar_top.tsx
+++ src/components/navbar_top.tsx
@@ -5,10 +5,10 @@ export default function NavbarTop() {
   return (
     <section className="bg-blue-950  text-white w-full  p-2 xl:px-1">
       <div className="container lg:text-xs text-[0.6rem] items-center gap-2 flex justify-start w-full  mx-auto pl-3 lg:pl-0">
-        <span className="flex items-center gap-1 ">
+        <span className="flex items-center gap-1 no-underline ">
           <HiMail />: sma_methodist_1_plg@gmail.com
         </span>
-        <span className="flex items-center gap-1">
+        <span className="flex items-center gap-1 no-underline">
           <HiPhone />: 0711-28814
         </span>
       </div>
