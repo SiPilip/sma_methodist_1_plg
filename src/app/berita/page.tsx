6838af6aa68ab5349329315e34dea8023f9ad98a
SiPilip <philifs.74hutar@gmail.com>
2025-08-22 20:21:49 +0700

    Menambahkan komponen Suspense pada halaman Berita untuk meningkatkan pengelolaan pemuatan konten dan pengalaman pengguna.

--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -1,11 +1,12 @@
 import BeritaList from "@/components/berita/berita";
 import HeroPage from "@/components/hero-page";
+import { Suspense } from "react";
 
 export default function Berita() {
   return (
-    <>
+    <Suspense>
       <HeroPage title="Berita" />
       <BeritaList />
-    </>
+    </Suspense>
   );
 }
